50d286e59097410824a4dd307221fc4339851881
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-06-14 11:48:50 +0900

    Add files via upload

--- src/model/ColorLogic.java
+++ src/model/ColorLogic.java
@@ -9,52 +9,42 @@ public class ColorLogic {
 
       int onOff = colorDB.get(select);
 
-	  //change onOff
-	  if (onOff == 0) {
-	      colorDB.set(select, 1);
+      //change onOff
+      if (onOff == 0) {
+          colorDB.set(select, 1);
 
-	  } else if (onOff == 1) {
-	      colorDB.set(select, 0);
-	  }
+      } else if (onOff == 1) {
+          colorDB.set(select, 0);
+      }
 
-	  matrixDB.setColorDB(colorDB);
+      matrixDB.setColorDB(colorDB);
 
-	  //---- Test Parameter ----
-	  //System.out.println(select);
-	  //System.out.println(onOff);
-	  //System.out.println(colorDB);
+      //---- Test Parameter ----
+      //System.out.println(select);
+      //System.out.println(onOff);
+      //System.out.println(colorDB);
 
-	  paintColor(select, colorDB);
+      return colorDB;
 
-	  return colorDB;
   }//ChangeColor()
 
   public List<String> paintColor(int select, List<Integer> colorDB) {
-	  List<String> color = new ArrayList<>(30);
+      List<String> color = new ArrayList<>(30);
       color.add(0,"none"); //マス番とリスト番号を合わせるためのダミー
 
       String onColor = "cornflowerblue";
-	  String offColor = "lavenderblush";
+      String offColor = "lavenderblush";
 
-	  for (int i = 1; i < colorDB.size(); i++) { //ダミーがあるので 1から
-	      //set colorDB to List "color"
-	      if (colorDB.get(i) == 0) {
-	      color.add(i , offColor);
+      for (int i = 1; i < colorDB.size(); i++) { //ダミーがあるので 1から
+          //set colorDB to List "color"
+          if (colorDB.get(i) == 0) {
+              color.add(i , offColor);
 
-	    	    } else if (colorDB.get(i) == 1) {
-	    	        color.add(i , onColor);
-	    	    }
-	    	}//for
+          } else if (colorDB.get(i) == 1) {
+              color.add(i , onColor);
+          }
+      }//for
 
-	  int onOff = colorDB.get(select);
-	  //set select
-	  if (onOff == 0) {
-	      color.set(select, onColor);
-
-	  } else if (onOff == 1) {
-	      color.set(select, offColor);
-	  }
-
-	  return color;
+      return color;
   }//paintColor()
 }//class
