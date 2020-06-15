09f4f516dd1720f01a05d85b6dedc8594af85ac9
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2020-06-15 17:03:53 +0900

    2020-06-15-1700

--- src/servlet/RegisterServlet.java
+++ src/servlet/RegisterServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import DAO.RegisterDAO;
+import model.RegisterLogic;
 import model.User;
 
 @WebServlet("/RegisterServlet")
@@ -25,6 +27,8 @@ public class RegisterServlet extends HttpServlet {
 		String action = request.getParameter("action");
 
 		if (action == null) {
+            String message = "";
+            request.setAttribute("message", message);
 
 			//register.jspへフォワード
 			String path = "/register.jsp";
@@ -34,23 +38,36 @@ public class RegisterServlet extends HttpServlet {
 		}else if(action.equals("done")) {
 
 			//セッションスコープの情報を取得する
-			HttpSession sesstion = request.getSession();
-			User user =(User) sesstion.getAttribute("user");
+			HttpSession session = request.getSession();
+			User user =(User) session.getAttribute("user");
 
-			//RegisterUserLogicのインスタンス化
-			RegisterUserLogic rul = new RegisterUserLogic();
-			rul.execute(user);//RegisterUserLogicの仮数値のためコメントアウト
+			//RegisterLogicのインスタンス化
+			RegisterLogic rl = new RegisterLogic();
+			boolean existAccountId = rl.execute(user);
 
-			sesstion.removeAttribute("user");
+			String message = "";
 
-			System.out.println("ok");
+			if (existAccountId) {
+				message = "そのアカウントＩＤはすでに使われています";
+				request.setAttribute("message", message);
 
-			//registerDone.jspへフォワード
-			String path = "/registerDone.jsp";
-			RequestDispatcher dis = request.getRequestDispatcher(path);
-			dis.forward(request, response);
+				String path = "/matrixGameIntegral/RegisterServlet";
+				RequestDispatcher dis = request.getRequestDispatcher(path);
+				dis.forward(request, response);
+			} else {
+				message = "登録ＯＫ";
+				//insert文でＤＢに登録
+				RegisterDAO dao = new RegisterDAO();
+				dao.insertRegister(user);
+
+				//forward で結果を表示
+			    String path = "/registerDone.jsp";
+			    RequestDispatcher dis = request.getRequestDispatcher(path);
+				dis.forward(request, response);
+			}//if else
+
 		}
-	}//get
+	}//doGet
 
 
 	protected void doPost(HttpServletRequest request,
@@ -59,22 +76,22 @@ public class RegisterServlet extends HttpServlet {
 	{
 		request.setCharacterEncoding("UTF-8");
 
-		String userId = request.getParameter("accountId");
+		String accountId = request.getParameter("accountId");
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
-		//RegisterUserクラスをインスタンス化
-		User User = new User(userId, name, pass);
+
+		//Userクラスをインスタンス化
+		User user = new User(accountId, name, pass);
 
 		//セッションスコープを使用する
 		HttpSession session = request.getSession();
-		session.setAttribute("User",User);
+		session.setAttribute("user",user);
 
 		//registerConfirm.jspへフォワード
 		String path = "/registerConfrim.jsp";
 		RequestDispatcher dis = request.getRequestDispatcher(path);
 		dis.forward(request, response);
 
-
-	}//post
+	}//doPost
 
 }
